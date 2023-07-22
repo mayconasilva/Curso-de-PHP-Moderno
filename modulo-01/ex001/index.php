92d116c8ffad79b514ccb8895c7ef456318b537c
Maycon Silva <73001893+mayconasilva@users.noreply.github.com>
2023-07-22 16:59:01 -0300

    Ex000 e Ex001 feitos

---
+++ modulo-01/ex001/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados do Servidor</title>
+    <link rel="stylesheet" href="../styles/main.css">
+</head>
+<body>
+    <h1>Dados do servidor</h1>
+
+    <?php phpinfo();?>
+    
+    
+</body>
+</html>
